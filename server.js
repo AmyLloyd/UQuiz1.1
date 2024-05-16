1a159d20f01668077593f7486e99cac403096208
Amy Lloyd <amylloyd30@gmail.com>
2024-05-16 13:55:55 +0930

    remove additional server script

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const routes = require('./controllers');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
