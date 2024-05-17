cab51c9d24e30f5f4f4a2b3b884083ae9691a8f6
Amy Lloyd <amylloyd30@gmail.com>
2024-05-17 10:37:34 +0930

    config JawsDB

--- controllers/api/categoryRoutes.js
+++ controllers/api/categoryRoutes.js
@@ -9,7 +9,7 @@ router.get('/', withAuth, async (req, res) => {
             res.status(404);
             res.json({ message: 'Category not found' });
         } else {
-            res.render('account', { categoryDisp }); // Pass the data to the view
+            res.render('profile', { categoryDisp }); // Pass the data to the view
         }
     } catch (err) {
         console.log(err);
@@ -18,12 +18,12 @@ router.get('/', withAuth, async (req, res) => {
 });
 
 // Create a route for adding categories on the backend
-router.post('/', async (req, res) => {
+router.post('/data/new', async (req, res) => {
     try {
         //Log the raw request body
-        console.log('Raw resuest body', req.body);
+        console.log('Raw request body', req.body);
         //Parse the JSON data
-        const categoryData = Category.create({
+        const categoryData = await Category.create({
             category_name: req.body.category_name,
         });
 
